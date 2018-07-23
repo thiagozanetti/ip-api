22a2ba17d6e9b5c47aa7787308443e424755a8c8
Thiago Zanetti <thiago.project@gmail.com>
2018-07-23 01:04:43 -0300

    Adding methods for quering ip location

---
+++ index.js
@@ -0,0 +1,13 @@
+const BASE_URL = 'http://ip-api.com'
+
+const fetch = window.fetch
+
+const baseFetch = type => url => fetch(`${BASE_URL}/${type}/${url}`)
+
+const csvFetch = baseFetch('csv')
+const jsonFetch = baseFetch('json')
+const xmlFetch = baseFetch('xml')
+
+export const csv = ip => csvFetch(ip).then(response => response.text())
+export const json = ip => jsonFetch(ip).then(response => response.json())
+export const xml = ip => xmlFetch(ip).then(response => response.text())
