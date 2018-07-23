00db24be21e503c68c3404d292d3009759e9b81e
Thiago Zanetti <thiago.project@gmail.com>
2018-07-23 01:10:20 -0300

    Adding status values

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const csvFetch = baseFetch('csv')
 const jsonFetch = baseFetch('json')
 const xmlFetch = baseFetch('xml')
 
+export const SUCCESS = 'success'
+export const FAIL = 'fail'
+
 export const csv = ip => csvFetch(ip).then(response => response.text())
 export const json = ip => jsonFetch(ip).then(response => response.json())
 export const xml = ip => xmlFetch(ip).then(response => response.text())
