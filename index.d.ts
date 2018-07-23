ca2f4ff4ed09aa7bf7617f3d04de7df664c4205a
Thiago Zanetti <thiago.project@gmail.com>
2018-07-23 01:04:52 -0300

    Adding type definition

---
+++ index.d.ts
@@ -0,0 +1,40 @@
+declare namespace ipApi {
+  const SUCCESS = 'success';
+  const FAIL = 'fail';
+
+  export type SUCCESS = typeof SUCCESS;
+  export type FAIL = typeof FAIL;
+
+  export type JSONLocation = {
+    as: string,
+    city: string,
+    country: string,
+    countryCode: string,
+    isp: string,
+    lat: number,
+    lon: number,
+    org: string,
+    query: string,
+    region: string,
+    regionName: string,
+    status: SUCCESS,
+    timezone: string,
+    zip: string
+  };
+
+  export type FailResponse = {
+    message: string,
+    query: string,
+    status: FAIL
+  };
+  
+  export type XMLLocation = XMLDocument;
+  export type CSVLocation = string;
+  
+  export function csv(ip: string): Promise<CSVLocation | FailResponse>;
+  export function json(ip: string): Promise<JSONLocation | FailResponse>;
+  export function xml(ip: string): Promise<XMLLocation | FailResponse>;  
+}
+
+export = ipApi;
+export as namespace ipApi;
